f7c18ae39e9754fec11dd7195df7f249f642d76d
Veera Pendyala <veera.pendyala@vpendyala-mbp.fireeye.com>
2015-08-30 20:40:26 -0700

    Third

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Client, ISO3166
+
+import datetime
 
 # Create your views here.
 def index(request, app=None, client_id=None):
-	import pdb; pdb.set_trace()
-
 	if app and 'clients' in app:
 		return clients(request)
 	if client_id:
@@ -16,16 +16,42 @@ def index(request, app=None, client_id=None):
 def clients(request):
 	country_dict = dict(ISO3166.ISO3166)
 	clients_list = [(x.id, x.client_name, country_dict[x.country], x.customer_site, x.start_date) for x in Client.objects.all()]
-	context = {'clients_list': clients_list}
+	context = {'clients_list': clients_list,
+			   'country_dict': ISO3166.ISO3166}
 	return render(request, 'main_app/clients.html', context)
+
+
+def save_client_app(request, client_id):
+	new_object = False
+	if client_id == 'add':
+		new_object = True
+		client_obj = Client()
+		client_obj.start_date = datetime.datetime.now()
+	else:
+		client_obj = Client.objects.get(id=client_id)
+
+	client_obj.client_name = request.POST['client_name']
+	client_obj.country = request.POST['country']
+	client_obj.customer_site = request.POST['customer_site']
+	client_obj.save()
+	return new_object, client_obj.id
 
 
 def client_details(request, client_id):
 	import pdb; pdb.set_trace()
+	new_object = False
+	if 'save_client_app' in request.POST and request.POST['save_client_app'] == '1':
+		new_object, client_id = save_client_app(request, client_id)
 
-	if client_id == 'add':
-		pass
+	if new_object:
+		return redirect('/clients/')
+
 	country_dict = dict(ISO3166.ISO3166)
 	client_obj = Client.objects.get(id=client_id)
-	context = {'client_details': client_obj}
-	return render(request, 'main_app/clients.html', context)
+	context = {'client_id': client_obj.id,
+		'client_name': client_obj.client_name,
+		'country': client_obj.country,
+		'customer_site': client_obj.customer_site,
+		'start_date': client_obj.start_date,
+		'country_dict': ISO3166.ISO3166}
+	return render(request, 'main_app/client_details.html', context)
